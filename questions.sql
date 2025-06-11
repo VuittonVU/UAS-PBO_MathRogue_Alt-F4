dcca9acfc58d37e82ff9ea09b326c76658317f2f
Sulung Ismanara <37434865+OrionZ12@users.noreply.github.com>
2025-06-11 12:52:06 +0700

    Delete questions.sql

--- questions.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO math_questions (question_id, type, template) VALUES
-('q1', 'red', 
- 'Seorang pedagang buah memiliki {x} buah apel. Ia membeli lagi {a} keranjang buah jeruk, dan setiap keranjang berisi {y} buah jeruk. Kemudian, ia berhasil menjual {b} buah apel dan {z} buah jeruk. Berapakah sisa buah apel dan jeruk yang dimiliki pedagang tersebut sekarang?'),
-
-('q2', 'red',
- 'Ibu ingin membuat kue kering untuk Lebaran. Satu resep kue kering membutuhkan {x} gram tepung terigu dan {y} gram gula pasir. Jika Ibu ingin membuat {z} resep kue kering, berapa total gram tepung terigu dan gula pasir yang dibutuhkan Ibu?'),
-
-('q3', 'blue',
- 'Sebuah tangga dengan panjang {x} meter disandarkan pada dinding. Sudut yang terbentuk antara ujung bawah tangga dengan permukaan tanah adalah {y}°. Berapakah tinggi dinding yang dapat dicapai oleh ujung atas tangga?');
